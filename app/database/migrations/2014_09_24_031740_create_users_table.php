8f72990c059f02da9d2cfef094b98acdde5c4dd9
jeraldpunx <jerald_patalinghug@yahoo.com>
2014-09-29 21:10:47 +0800

    Revert "Sep 27 - 1 AM"
    This reverts commit 6c76c761f3fc89a5a655db3d505f2bf1f3ad6b64.

--- app/database/migrations/2014_09_24_031740_create_users_table.php
+++ app/database/migrations/2014_09_24_031740_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
+			$table->string('email', 50)->unique();
 			$table->string('username', 20)->unique();
 			$table->string('password', 60);
 			$table->string('first_name');
 			$table->string('last_name');
+			$table->date('birthdate');
 			$table->integer('previlage');
 			$table->timestamps();
 		});
--- app/routes.php
+++ app/routes.php
@@ -1,12 +1,17 @@
 <?php
 
-Route::get('/', ['as'=>'home', function(){
-	return View::make('home');
-}]);
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
 
-Route::get('login', array('as'=>'login', 'uses'=>'UsersController@showLogin'))->before('guest');
-Route::post('login', 'UsersController@doLogin');
-Route::get('logout', ['as'=>'logout', 'uses'=>'UsersController@logout'])->before('auth');
-Route::post('logout', 'UsersController@logout');
-Route::get('register', 'UsersController@create')->after('guest');
-Route::post('register', 'UsersController@store');
+Route::get('/', function()
+{
+	return View::make('hello');
+});
