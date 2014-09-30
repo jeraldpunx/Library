a4ef8c9ca3cce64f2c00ad2724bf081036341c5c
jeraldpunx <jerald_patalinghug@yahoo.com>
2014-09-30 15:13:52 +0800

    Tabada

--- app/views/include/nav.blade.php
+++ app/views/include/nav.blade.php
@@ -9,29 +9,11 @@
 			</button>
 			<a href="{{ URL::route('home') }}" class="navbar-brand">{{$companyName}}</a>
 		</div>
-
 <!-- CHECK IF LOGIN -->
 @if(Auth::check())
 	<!-- CHECK IF ADMIN OR NORMAL -->
 	@if(Auth::user()->previlage == 0)
 		<div class="navbar-collapse collapse">
-			<ul class="nav navbar-nav">
-				<li><a href="{{ URL::route('home') }}">Home</a></li>
-				<li><a href="#about">Profile</a></li>
-				<li><a href="#contact">Issue Books</a></li>
-				<li class="dropdown">
-					<a data-toggle="dropdown" class="dropdown-toggle" href="#">Dropdown <span class="caret"></span></a>
-					<ul role="menu" class="dropdown-menu">
-						<li><a href="#">Action</a></li>
-						<li><a href="#">Another action</a></li>
-						<li><a href="#">Something else here</a></li>
-						<li class="divider"></li>
-						<li class="dropdown-header">Nav header</li>
-						<li><a href="#">Separated link</a></li>
-						<li><a href="#">One more separated link</a></li>
-					</ul>
-				</li>
-			</ul>
 			<ul class="nav navbar-nav navbar-right">
 				<li class="dropdown">
 					<a data-toggle="dropdown" class="dropdown-toggle" href="#"><i class="fui-user"></i> {{Auth::user()->username}}<span class="caret"></span></a>
